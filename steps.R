e9fd7c01e3dc533371ed2c8e392565c8d56c5dbe
richlaverty <rich.laverty@gmail.com>
2022-04-05 16:58:51 -0400

    histogram of total steps per day added

--- steps.R
+++ steps.R
@@ -12,3 +12,17 @@ if(file.exists("./data/repdata_data_activity/activity.csv"))
 {
   activity <- read.csv("./data/repdata_data_activity/activity.csv")
 }
+
+# there are 3 columns to the data
+# 1. steps  - an integer count of the steps taken
+# 2. date - a "NULL" class giving the date of the data in YYYY-MM-DD format
+# 3. interval - an integer giving the minutes since midnight
+
+# make entries in the date column members of the "Date" class
+# activity$date <- as.Date(as.character(activity$date))
+
+# get the sum of steps taken each day:
+library(dplyr)
+byday <- group_by(activity, date)
+totalSteps <- summarize(byday, total = sum(steps))
+hist(totalSteps$total)
